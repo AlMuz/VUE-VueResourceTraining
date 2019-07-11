6cddbd5a098fc47df59fee47648fa92cf0f71ada
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-07-11 15:23:27 +0300

    vue-resource added and first post request to the firebase

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                  <label>Username</label>
+                  <input class="form-control" type="text" v-model="user.username">
+                </div>
+
+                <div class="form-group">
+                  <label>Email</label>
+                  <input class="form-control" type="email" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
             </div>
         </div>
     </div>
@@ -10,6 +20,24 @@
 
 <script>
     export default {
+      data() {
+        return {
+          user: {
+            username: '',
+            email: '',
+          }
+        }
+      },
+      methods: {
+        submit() {
+          this.$http.post('https://vuejs-http-30886.firebaseio.com/data.json', this.user)
+            .then(response => {
+              console.log(response);
+            }, error => {
+              console.log(error);
+            });
+        }
+      }
     }
 </script>
 
